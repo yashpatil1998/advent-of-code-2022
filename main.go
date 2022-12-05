91a272a6e892edef79c5e0ef56f2126b55405fe5
yashpatil1998 <yashpatil1998@gmail.com>
2022-12-05 10:33:22 +0530

    Day3 solutions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"AdventOfCode2022/day1"
 	"AdventOfCode2022/day2"
+	"AdventOfCode2022/day3"
 	"log"
 )
 
@@ -11,4 +12,6 @@ func main() {
 	log.Printf("day1 part 1 %v part2 %v", day1p1, day1p2)
 	day2p1, day2p2 := day2.Day2Logic()
 	log.Printf("day2 part 1 %v part2 %v", day2p1, day2p2)
+	day3p1, day3p2 := day3.Day3Logic()
+	log.Printf("day3 part 1 %v part2 %v", day3p1, day3p2)
 }
